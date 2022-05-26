2ce4da2e8649b3a2a47423519725fb4adefceca9
naveenraja1804 <naveenraja1804@gmail.com>
2022-05-26 21:42:31 +0530

    new msg

--- src/test/java/org/test/Sample.java
+++ src/test/java/org/test/Sample.java
@@ -9,4 +9,12 @@ private void test1() {
 	// TODO Auto-generated method stub
 
 }
+private void test2() {
+	// TODO Auto-generated method stub
+
+}
+private void test4() {
+	// TODO Auto-generated method stub
+
+}
 }
